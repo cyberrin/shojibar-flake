8f56c0e3a8211746f72605e8d529b4388330f03f
cyberrin <cyberrin@tuta.io>
2026-08-29 22:06:16 +0500

    added nixos module

--- flake.nix
+++ flake.nix
@@ -146,6 +146,24 @@
         };
       }
     );
+    nixosModules.default = { config, lib, pkgs, ... }:
+    let
+      cfg = config.programs.shoji-bar-2;
+    in {
+      options.programs.shoji-bar-2 = {
+        enable = lib.mkEnableOption "the Shoji Bat 2 desktop shell";
+        package = lib.mkOption {
+          type = lib.types.package;
+          default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+          description = "The shoji-bar-2 package to use.";
+        };
+      };
+
+      config = lib.mkIf cfg.enable {
+        environment.systemPackages = [ cfg.package pkgs.ags ];
+        services.upower.enable = lib.mkDefault true;
+        services.power-profiles-daemon.enable = lib.mkDefault true;
+      };
+    };
   };
 }
-
